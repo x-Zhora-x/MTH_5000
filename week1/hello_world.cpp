0436d691db23dee839d3af7cfea9e5372262b30c
x-Zhora-x <dshmir04@gmail.com>
2026-09-08 03:13:07 -0400

    Update hello_world.cpp

--- week1/hello_world.cpp
+++ week1/hello_world.cpp
@@ -2,5 +2,25 @@
 
 int main() {
     std::cout << "oi buddy\n";
+
+    bool a = true;
+    char b = 'b';
+    int c = 3;
+    double d = 3.0;
+    unsigned e = 5;
+
+    std::cout << sizeof(a) << std::endl;
+    std::cout << sizeof(b) << std::endl;
+    std::cout << sizeof(c) << std::endl;
+    std::cout << sizeof(d) << std::endl;
+    std::cout << sizeof(a) << std::endl;
+
+    std::cout << "address of d in hex:" << &d << std::endl;
+    // Going to point to this guy and store this address!
+
+    double* float_pointer;
+    float_pointer = &d; // Now ive stored the adress
+
+
     return 0;
 }
